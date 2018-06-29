9f86cb53b68306f5a54c295d925fdb06da041f70
Thiago Candido <thiiagohenriqui@gmail.com>
2018-06-29 00:36:21 -0300

    fix: Corrige import do componente Header

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-// import Header from './components/header/header';
+import Header from './components/header/header';
 import Main from './main';
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <Header /> */}
+        <Header />
         <Main />
       </div>
     );
