76d91ae4814a9a3dde2a54a4ef7c060aad7cf6f7
Thiago Candido <thiiagohenriqui@gmail.com>
2018-06-29 00:24:05 -0300

    feat: Cria componente do Header

---
+++ portfolio/src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar, Row} from 'react-materialize';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => (
+    <Row>  
+        <Navbar className="grey darken-2">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="contact">Contact</NavLink></li>
+        </Navbar>
+    </Row>
+);
+
+export default Header;
