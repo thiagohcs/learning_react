8f884a5262a1908f50ba8c5d5edfb86db673c495
Thiago Candido <thiiagohenriqui@gmail.com>
2018-06-29 00:19:47 -0300

    initial commit

---
+++ portfolio/src/main.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Home from "./components/home/home";
+import Contact from "./components/contact/contact";
+import NotFound from "./components/not-found/not-found";
+import { Container } from 'react-materialize';
+import { Switch, Route } from 'react-router-dom';
+
+const Main = () => (
+    <main>
+        <Container>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/contact' component={Contact} />
+                <Route path="*" component={NotFound} />
+            </Switch>
+        </Container>
+    </main>
+);
+
+export default Main;
