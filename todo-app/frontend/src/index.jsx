81efd81aed253af9465e8bd92e23a6e80ee388c3
Thiago H. Candido <thiiagohenriqui@gmail.com>
2018-08-05 13:23:07 -0300

    chore(config): Configure redux dev tools

--- todo-app/frontend/src/index.jsx
+++ todo-app/frontend/src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './main/app'
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducers from './main/reducers'
+import promise from 'redux-promise'
 
-const store  = createStore(reducers)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store  = applyMiddleware(promise)(createStore)(reducers, devTools)
 
 ReactDOM.render(
     <Provider store={store} >
