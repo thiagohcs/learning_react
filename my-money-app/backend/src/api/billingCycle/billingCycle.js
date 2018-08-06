54e30ededd0f78472076fbba95a3ac85bed4c0ef
Thiago H. Candido <thiiagohenriqui@gmail.com>
2018-08-06 00:51:20 -0300

    feat: Create validations of the models

--- my-money-app/backend/src/api/billingCycle/billingCycle.js
+++ my-money-app/backend/src/api/billingCycle/billingCycle.js
@@ -1,27 +1,27 @@
 const restful = require('node-restful')
-const mongoogse = restful.mongoogse
+const mongoose = restful.mongoose
 
-const creditSchema = new mongoogse.creditSchema({
+const creditSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'Informe o nome do crédito']
     },
     value: {
         type: Number,
         min: 0,
-        required: true
+        required: [true, 'Informe o valor do crédito!']
     }
 })
 
-const debitSchema = new mongoogse.debitSchema({
+const debitSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'Informe o valor do débito!']
     },
     value: {
         type: Number,
         min: 0,
-        required: true
+        required: [true, 'Informe o valor do débito!']
     },
     status: {
         type: String,
@@ -31,22 +31,22 @@ const debitSchema = new mongoogse.debitSchema({
     }
 })
 
-const billingCycleSchema = new mongoogse.billingCycleSchema({
+const billingCycleSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'Informe uma descrição para o ciclo de pagamento!']
     },
     month: {
         type: Number,
         min: 1,
         max: 12,
-        required: true
+        required: [true, 'Informe o mês do ciclo de pagamento']
     },
     year: {
         type: Number,
         min: 1970,
         max: 2100,
-        required: true
+        required: [true, 'Informe o ano do ciclo de pagamento']
     },
     credits: [creditSchema],
     debits: [debitSchema]
