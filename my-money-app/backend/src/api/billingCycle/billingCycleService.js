9f1af6744a77c1c06d8c1cef2144f3578b0ae019
Thiago H. Candido <thiiagohenriqui@gmail.com>
2018-08-06 02:00:13 -0300

    feat: Create tratament of the errors

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -1,10 +1,9 @@
 const BillingCycle = require('./billingCycle')
+const errorHandler = require('../common/errorHandler')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({
-    new: true,
-    runValidators: true
-})
+BillingCycle.updateOptions({ new: true, runValidators: true })
+BillingCycle.after('post', errorHandler).after('put', errorHandler)
 
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value) => {
@@ -19,7 +18,6 @@ BillingCycle.route('count', (req, res, next) => {
         }
     })
 })
-
 
 BillingCycle.route('summary', (req, res, next) => {
     BillingCycle.aggregate([{
