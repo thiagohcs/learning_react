3d6f4aefe2fd9c5b1c4d4c1b889febb46c199db5
Thiago H. Candido <thiiagohenriqui@gmail.com>
2018-08-13 02:08:48 -0300

    feat: Create template of dashboard

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy 2018
+            <a href="#"> Thiago H. Candido</a>
+        </strong>
+    </footer>
+)
