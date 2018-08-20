0d7b38a622c8c622e90265151cae0544a0c65890
Thiago H. Candido <thiiagohenriqui@gmail.com>
2018-08-20 00:37:22 -0300

    feat: Cria serviço de  autenticação no servidor

--- my-money-app/backend/src/config/routes.js
+++ my-money-app/backend/src/config/routes.js
@@ -1,11 +1,29 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
 
-    const router = express.Router()
-    server.use('/api', router)
+    /*
+    * Rotas protegidas por Token JWT
+    */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
 
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    BillingCycle.register(protectedApi, '/billingCycles')
+
+
+    /*
+    * Rotas abertas
+    */
+    const openApi = express.Router()
+    server.use('/opai', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/singup', AuthService.singup)
+    openApi.post('/validateToken', AuthService.validateToken)
     
 }
