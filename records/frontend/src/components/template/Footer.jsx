9c132d3967e88d590686ba5913d162b51edfb73e
Thiago H. Candido <thiiagohenriqui@gmail.com>
2018-09-03 00:54:31 -0300

    feat: Cria frontend do cadastro de Usuário

---
+++ records/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+import React from 'react';
+
+export default props => (
+    <footer className='footer'>
+        <span>
+            Desenvolvido com <i className='fa fa-heart text-danger'></i> por 
+            <strong className='text-danger'> Thiago H. Candido</strong>
+        </span>
+    </footer>
+);
