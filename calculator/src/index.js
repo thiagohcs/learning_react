b3960511355a482a9a4c26fe510815cec521acc0
Thiago H. Candido <thiiagohenriqui@gmail.com>
2018-09-03 00:56:07 -0300

    feat: Cria calculadora em React

---
+++ calculator/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './main/Calculator';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    <Calculator />
+, document.getElementById('root'));
+
+registerServiceWorker();
